a1ccd1620fa1a6ddcee9f30280438393a3aff47f
Eom <righthyeon00@gmail.com>
2024-09-18 11:27:16 +0900

    m3u8 url to save image 날짜별 저장

--- m3u8_to_save_image_240918_001.py
+++ m3u8_to_save_image_240918_001.py
@@ -4,33 +4,30 @@ import time
 from datetime import datetime, timedelta
 
 
-# .m3u8 파일 URL
-
+# test
 # video_url = 'https://strm2.spatic.go.kr/live/152.stream/playlist.m3u8'
+# video_name = 'test'
+# save_interval = 15
+# save_quality = 80
 
 # 서부역 입구 삼거리 seobuyeog ibgu samgeoli
 # video_url = 'https://wowza.cheonan.go.kr/live/cctv002.stream/playlist.m3u8'
 # video_name = 'seobuyeog_ibgu_samgeoli'
 # save_interval = 15
+# save_quality = 80
 
 # 학성 중학교 hagseong junghaggyo
 video_url = 'http://211.34.248.240:1935/live/T065.stream/playlist.m3u8'
 video_name = 'hagseong_junghaggyo'
 save_interval = 10
+save_quality = 90
 
 
-def m3u8_to_save_img(video, name, frame_interval):
+def m3u8_to_save_img(video, name, interval, quality):
     frame_count = 0
-    saved_count = 0
-
-    # 프레임 저장 폴더
-    save_dir = f'./{name}'
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-        print(name, '저장 폴더 생성')
+    current_day = None
 
     while True:
-        current_time = datetime.now().replace(microsecond=0)
         try:
             cap = cv2.VideoCapture(video)
             if not cap.isOpened():
@@ -44,14 +41,26 @@ def m3u8_to_save_img(video, name, frame_interval):
                     print("스트리밍이 중단되었습니다. 다시 시작합니다...")
                     break
 
+                # 현재 날짜 확인
+                now = datetime.now()
+                today = now.strftime("%Y-%m-%d")
+
+                # 날짜 변경 폴더 생성
+                if current_day != today:
+                    current_day = today
+                    save_dir = f'./{name}/{current_day}'
+                    if not os.path.exists(save_dir):
+                        os.makedirs(save_dir)
+                        print(f'{save_dir} 저장 폴더 생성')
+
                 # 15프레임마다 한 장 저장
-                if frame_count % frame_interval == 0:
+                if frame_count % interval == 0:
+                    current_time = datetime.now()
                     formatted_time = current_time.strftime("%Y-%m-%d_%H-%M-%S_%f")
                     frame_filename = os.path.join(save_dir, f'{formatted_time}.jpg')
-                    cv2.imwrite(frame_filename, frame, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
+                    cv2.imwrite(frame_filename, frame, [int(cv2.IMWRITE_JPEG_QUALITY), quality])
                     print(f'Saved {frame_filename}')
-                    saved_count += 1
-                    current_time = current_time + timedelta(seconds=0.500001)
+                    frame_count = frame_count - interval
 
                 frame_count += 1
 
@@ -64,5 +73,5 @@ def m3u8_to_save_img(video, name, frame_interval):
 
 
 if __name__ == "__main__":
-    m3u8_to_save_img(video_url, video_name, save_interval)
-    
+    m3u8_to_save_img(video_url, video_name, save_interval, save_quality)
+
